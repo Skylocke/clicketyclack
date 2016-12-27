5f51a83baef7ca7c07f0d5bdd2a7012b4962b6b6
skylocke <mathematical.fiction@gmail.com>
2016-12-27 13:39:40 -0800

    why won't my POST stuff work gdmt a;sldkfja :|

--- index.js
+++ index.js
@@ -17,14 +17,54 @@ app.use(express.static(path.join(__dirname, "static")));
 
 /* ROUTES */
 // GET /
+// view: views/welcome.ejs
+// purpose: homepage, asks if user is new to site and directs them to clicker creation form
+app.get("/", function(req, res) {
+  res.render("site/welcome");
+});
 
 // GET /new
-// have a
+// view: views/everyone/new.ejs
+// purpose: displays form for creating new clicker
+app.get("/new", function(req, res) {
+  res.render("everyone/new");
+});
+
+// POST /new
+// view: none (redirects to /everyone after the clicker is created)
+// purpose: creates a new clicker
+app.post("/new", function(req, res) {
+  console.log(req.body);
+  db.clickers.create(req.body).then(function(clicker) {
+    console.log(clicker);
+    res.redirect("/everyone");
+  });
+});
 
 // GET /everyone
+// view: views/everyone/index.ejs
+// purpose: displays all clickers and clicks (ranked by clicks, hopefully, or modular later)
+app.get("/everyone", function(req, res) {
+  db.clickers.findAll({ order: "clicks" }).then(function(clickers) {
+    res.render("everyone/index", { clickers: clickers });
+  });
+});
 
 // GET /everyone/:name
+// view: views/everyone/show.ejs
+// purpose: displays specific clicker by name and their clickety stats
+app.get("/everyone/:name", function(req, res) {
+  db.clickers.findAll({ where: { name: req.params.name } }).then(function(clicker) {
+    console.log("name = ", clicker[0].dataValues)
+    clicker = clicker[0].dataValues;
+    res.render("everyone/show", { clicker: clicker });
+  });
+});
 
 // GET /everyone/:name/sprint
 
 // GET /everyone/:name/endurance
+
+
+// HEY, LISTEN!
+app.listen(3000);
